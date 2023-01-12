07150e4555dafc2510cb4f013dd13e720ce369a7
Imhotep <imhotep.php@gmail.com>
2023-01-12 15:43:29 +0600

    [Container] Initial component

---
+++ src/Container/ContainerException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Imhotep\Container;
+
+use Psr\Container\ContainerExceptionInterface;
+
+class ContainerException extends \Exception implements ContainerExceptionInterface
+{
+
+}
